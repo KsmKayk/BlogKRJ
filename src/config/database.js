25c034811b73ba8b0b2cc76376175a75dcd13a64
Kayk <ksm.kayk@outlook.com>
2019-08-30 18:53:03 -0300

    adicionado o basico --  criado servidor, base da database

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: "mysql",
+  host: "127.0.0.1",
+  username: "root",
+  password: "root",
+  database: "blog_krj",
+  operatorAliases: false,
+  difine: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
